b05aba8de1ed5ea3610faee81604283a6a81d970
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-20 00:09:33 +0000

    Added new assets

---
+++ assets/maps/tiled/jamegam.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="jamegam" tilewidth="40" tileheight="40" tilecount="9" columns="3">
+ <image source="jamegam.png" width="120" height="120"/>
+ <tile id="0">
+  <properties>
+   <property name="TileType" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="TileType" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="TileType" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="TileType" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="TileType" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="TileType" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="TileType" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="TileType" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="TileType" type="int" value="0"/>
+  </properties>
+ </tile>
+</tileset>
